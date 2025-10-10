e76cf979f64bb45bd5e73536f94a35d3712d1c8b
Prabhat4a <rs1350214@gmail.com>
2025-10-10 23:50:09 +0530

    Update UPGRADED PAGE.js
    i added some java script codes in my site

---
+++ REFUIRBISH/UPGRADED PAGE.js
@@ -0,0 +1,7 @@
+// Select the login and register containers
+const loginBox = document.querySelector(".objects");
+const registerBox = document.querySelector(".objects-register");
+
+// Select the toggle links
+const createAccLink = document.querySelector(".creat-acc");
+const loginAccLink = document.querySelector(".Login-acc");
